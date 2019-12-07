f827baeb08a6077be8e868d5f7ac858213c470f7
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-07 00:12:02 -0600

    Using TheNavbar component as main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <the-navbar></the-navbar>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import TheNavbar from "@/components/TheNavbar.vue"
+  export default {
+    components: {TheNavbar}
+  }
+</script>
