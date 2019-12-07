7cc5c0e004fa46babe327ca2d135ba231dc90f32
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-07 00:13:38 -0600

    Define main hero layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+  <div class="hero is-dt-primary">
+    <div class="hero-body">
+      <h2 class="title is-2 has-text-centered">
+        Centraliza la documentacion de todo tu equipo <br>en un solo lugar y gestionalo en todas partes
+      </h2>
+      <img src="@/assets/welcome.svg" alt="welcome to documentive" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+  img {
+    width: 90%;
+    height: 60vh;
+  }
+</style>
