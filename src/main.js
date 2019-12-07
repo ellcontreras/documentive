b26847bed9edb7cc10fbfa391be29e123761db1d
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-06 22:46:36 -0600

    Add firebase into project

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import firebase from "firebase"
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCnXJigrILkSbRHWmMRaVpVgekY76ep6e4",
+  authDomain: "documentive-da37d.firebaseapp.com",
+  databaseURL: "https://documentive-da37d.firebaseio.com",
+  projectId: "documentive-da37d",
+  storageBucket: "documentive-da37d.appspot.com",
+  messagingSenderId: "712719294772",
+  appId: "1:712719294772:web:05fbe9ced78af581018561",
+  measurementId: "G-SJNTCRTNP7"
+})
+
+firebase.analytics()
 
 new Vue({
   router,
