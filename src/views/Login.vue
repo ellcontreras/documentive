3c01b3df805d5670747fdb29ecd1805937d5f06e
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-13 18:27:30 -0600

    Create login view

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <Center>
+            <h2 class="subtitle is-2">Iniciar Sesión</h2>
+            <the-login-button></the-login-button>
+        </Center>
+    </div>
+</template>
+
+<script>
+import TheLoginButton from "@/components/TheLoginButton.vue"
+import Center from "@/layouts/Center.vue"
+
+export default {
+    name: 'Login',
+    components: { TheLoginButton, Center }
+}
+</script>
