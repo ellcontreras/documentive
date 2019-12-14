a6cef62eef1da63f00123cc33aa82fe468174a69
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-13 18:34:10 -0600

    Removing center layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="container">
-        <Center>
+        <section class="section">
             <h2 class="subtitle is-2">Iniciar Sesión</h2>
             <the-login-button></the-login-button>
-        </Center>
+        </section>
     </div>
 </template>
 
 <script>
 import TheLoginButton from "@/components/TheLoginButton.vue"
-import Center from "@/layouts/Center.vue"
 
 export default {
     name: 'Login',
-    components: { TheLoginButton, Center }
+    components: { TheLoginButton }
 }
 </script>
