eaa2b60a8b8d032f44c751a053834bc4ad834469
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-13 18:40:35 -0600

    Using base container and base form card layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="container">
-        <section class="section">
+    <base-container>
+        <base-form-card>
             <h2 class="subtitle is-2">Iniciar Sesión</h2>
             <the-login-button></the-login-button>
-        </section>
-    </div>
+        </base-form-card>
+    </base-container>
 </template>
 
 <script>
 import TheLoginButton from "@/components/TheLoginButton.vue"
+import BaseContainer from "@/layouts/BaseContainer.vue"
+import BaseFormCard from "@/layouts/BaseFormCard.vue"
 
 export default {
     name: 'Login',
-    components: { TheLoginButton }
+    components: { TheLoginButton, BaseFormCard, BaseContainer }
 }
 </script>
