fc6ef86813e5bc7d2d1dd2a4787bc2b6857d4cd2
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-15 14:29:16 -0600

    Adding constants and user state

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { UPDATE_USER_STATE } from "./constants";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    logedIn: false,
+    user: []
   },
   mutations: {
+    [UPDATE_USER_STATE]: (state, payload) => {
+      state.user = payload;
+    }
   },
   actions: {
+    [UPDATE_USER_STATE]: ({ commit }) => {
+      commit(UPDATE_USER_STATE)
+    }
   },
   modules: {
   }
