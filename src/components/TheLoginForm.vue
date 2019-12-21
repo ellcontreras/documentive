19f4d7c39c579e3628a5a3460136df292f4456b6
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-20 19:10:59 -0600

    Create Login Form component

---
+++ src/components/TheLoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="section">
+         <b-field label="Email">
+            <b-input v-model="email" type="email"></b-input>
+        </b-field>
+        <b-field label="password">
+            <b-input v-model="password" type="password" password-reveal></b-input>
+        </b-field>
+
+        <b-button 
+            @click="handler(email, password)"
+            type="is-dt-primary">
+                {{ label }}
+            </b-button>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'TheLoginForm',
+    props: {
+        label:{
+            required: true
+        },
+        handler: {
+            required: true
+        }
+    },
+    data: () => ({
+        email: '',
+        password: ''
+    })
+}
+</script>
