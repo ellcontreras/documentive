822c4a7dc53dd78ef54bd2b893dccc97d2263383
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-20 19:11:20 -0600

    Update Login view by use TheLoginForm component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,49 @@
 <template>
     <base-container>
         <base-form-card>
-            <h2 class="subtitle is-2 has-text-centered">Iniciar Sesión</h2>
-            <the-login-button class="btn-center"></the-login-button>
+            <h2 class="subtitle is-2 has-text-centered">
+                {{ $route.params.type || "Iniciar Sesión" }}
+            </h2>
+            <the-login-form
+                :handler="onSubmit"
+                :label="$route.params.type || 'Iniciar Sesión'"></the-login-form>
         </base-form-card>
     </base-container>
 </template>
 
 <script>
-import TheLoginButton from "@/components/TheLoginButton.vue"
 import BaseContainer from "@/layouts/BaseContainer.vue"
 import BaseFormCard from "@/layouts/BaseFormCard.vue"
+import TheLoginForm from "@/components/TheLoginForm.vue"
+
+import { mapGetters } from "vuex";
 
 export default {
     name: 'Login',
-    components: { TheLoginButton, BaseFormCard, BaseContainer }
+    components: { TheLoginForm, BaseFormCard, BaseContainer },
+    data: () => ({
+        email: '',
+        password: ''
+    }),
+    computed: {
+        ...mapGetters(['user']),
+        nextRoute() {
+            return this.$route.query.redirect || '/'
+        }
+    },
+    watch: {
+        user(auth) {
+            if (!!auth) {
+                this.$router.replace(this.nextRoute)
+            }
+        }
+    },
+    methods: {
+        async onSubmit(email, password) {
+            let type = this.$route.params.type == "Registro" ? "register" : "login";
+            const auth = await this.$auth.login(email, password, type);
+        }
+    }
 }
 </script>
 
