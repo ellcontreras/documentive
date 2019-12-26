5009ea340123b58527d751a2369486d5cb7fec7c
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-26 01:51:42 -0600

    Use Chart and Motd inside App view

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,3 +1,105 @@
 <template>
-    <h1 class="title is-1 has-text-centered">Welcome to Documentive!</h1>
+  <div>
+    <div class="background"></div>
+    <div class="container">
+      <div class="columns has-text-dt-gray">
+        <div class="column is-one-third">
+          <figure class="image is-128x128">
+            <img
+              class="is-rounded"
+              src="https://bulma.io/images/placeholders/128x128.png"
+            />
+          </figure>
+          <strong class="has-text-centered block is-marginless has-text-dt-gray"
+            >Luis Contreras Guerra</strong
+          >
+          <small class="has-text-centered block">Software Developer</small>
+
+          <div class="columns has-text-centered">
+            <div class="column is-one-third block">
+              <b-icon icon="user"></b-icon>
+            </div>
+            <div class="column is-one-third block">
+              <b-icon icon="comments"></b-icon>
+            </div>
+            <div class="column is-one-third block">
+              <b-icon icon="sticky-note"></b-icon>
+            </div>
+          </div>
+
+          <div class="columns has-text-centered">
+            <div class="column is-half">
+              <b-icon icon="home" size="is-large"></b-icon>
+              <strong class="block has-text-dt-gray">Home</strong>
+            </div>
+            <div class="column is-half">
+              <b-icon icon="user" size="is-large"></b-icon>
+              <strong class="block has-text-dt-gray">Profile</strong>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <the-motd></the-motd>
+          <chart
+            :options="options"
+            :data="series"
+            height="350px"
+            title="Datos de prueba"
+          ></chart>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  import TheMotd from "@/components/TheMotd.vue";
+  import Chart from "@/components/Chart.vue";
+
+  export default {
+    name: "App",
+    components: { TheMotd, Chart },
+    data: () => ({
+      options: {
+        labels: ["Apples", "Oranges", "Berries", "Grapes"]
+      },
+      series: [
+        {
+          name: "hola",
+          data: [1, 1.9, 2, 3]
+        }
+      ]
+    })
+  };
+</script>
+
+<style scoped>
+  .background {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    pointer-events: none;
+    height: 430px;
+    background-color: #6e00ff;
+    border-bottom-left-radius: 2.5rem;
+  }
+
+  .chart {
+    width: 100%;
+    height: 30px !important;
+  }
+
+  figure {
+    margin: auto;
+  }
+
+  .card {
+    border-radius: 10px;
+  }
+
+  .block {
+    display: block;
+  }
+</style>
