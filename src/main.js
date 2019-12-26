f15df8d530d3d9fadf8491f486f422e1604d25a7
Luis Contreras Guerra <luis@mailvibes.io>
2019-12-26 01:52:55 -0600

    Import ApexCharts

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
-import Vue from 'vue'
-import Index from './Index.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import Buefy from 'buefy'
+import Vue from "vue";
+import Index from "./Index.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import Buefy from "buefy";
+import VueApexCharts from "vue-apexcharts";
 
-import 'buefy/dist/buefy.css'
-import './assets/theme.scss'
-import FirebaseAuthPlugin from './FirebaseAuthPlugin'
+import "buefy/dist/buefy.css";
+import "./assets/theme.scss";
+import FirebaseAuthPlugin from "./FirebaseAuthPlugin";
 
 Vue.use(Buefy, {
-  defaultIconPack: 'fas',
-})
+  defaultIconPack: "fas"
+});
 
-Vue.config.productionTip = false
+Vue.component("apexchart", VueApexCharts);
 
-Vue.use(FirebaseAuthPlugin)
+Vue.config.productionTip = false;
+
+Vue.use(FirebaseAuthPlugin);
 
 new Vue({
   router,
   store,
   render: h => h(Index)
-}).$mount('#app')
+}).$mount("#app");
